a99c19d1a4d3d7ac8c13b5aff3cb147d8d2ed46f
ttalgihon <vivithefool@gmail.com>
2019-03-12 08:44:41 -0400

    Create SQL data

--- puppyProfile.sql
+++ puppyProfile.sql
@@ -1,36 +1,21 @@
-DROP DATABASE IF EXISTS `puppy_info`;
-CREATE DATABASE `puppy_info`;
-USE `puppy_info`;
-CREATE TABLE `puppy_info` (
+-- Schema.sql for SQL database/table
+DROP DATABASE IF EXISTS puppy_info;
 
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE DATABASE puppy_info;
+USE puppy_info;
 
-  `first_name` varchar(45) NOT NULL,
-
-  `last_name` varchar(45) DEFAULT NULL,
-
-  `email` varchar(45) NOT NULL,
-
-  `puppy_name` varchar(45) NOT NULL,
-
-  `puppy_gender` varchar(45) NOT NULL,
-
-  `puppy_breed` varchar(45) NOT NULL,
-
-  `address` varchar(45) DEFAULT NULL,
-
-  `zipcode` int(11) NOT NULL,
-
-  `puppy_personality` varchar(45) DEFAULT NULL,
-
-  `puppy_age` int(11) NOT NULL,
-
-  `puppy_weight` int(11) NOT NULL,
-
-  `img_url` longtext,
-
-  PRIMARY KEY (`id`),
-
-  UNIQUE KEY `email_UNIQUE` (`email`));
-
-  
+-- Create a new table with left-right column titles
+CREATE TABLE puppy_info (
+  id INT(15) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(45) NOT NULL,
+  last_name VARCHAR(45) NOT NULL,
+  email VARCHAR(45) NOT NULL UNIQUE,
+  meet_location VARCHAR(45) NOT NULL,
+  puppy_name VARCHAR(45) NOT NULL,
+  puppy_gender VARCHAR(45) NOT NULL,
+  puppy_breed VARCHAR(45) NOT NULL,
+  puppy_personality VARCHAR(45) DEFAULT NULL,
+  puppy_age INT(100) NOT NULL,
+  puppy_weight INT(100) NOT NULL,
+  img_url longtext
+);
